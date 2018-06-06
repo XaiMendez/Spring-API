2c61e74bd2d520fd633d8f274a294ebde7bd3970
XaiMendez <bmendez@bancoazul.com>
2018-06-06 15:18:03 -0600

    Adding hibernate functions

--- src/main/java/com/tminnova/api/repository/UserRepository.java
+++ src/main/java/com/tminnova/api/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.tminnova.api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +10,10 @@ import com.tminnova.api.model.User;
 
 @Component
 @Transactional
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	List<User> findAll();
-
-
+	
+	User findById(int id);
+	
 }
