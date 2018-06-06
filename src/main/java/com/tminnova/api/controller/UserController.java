aaeb10b877027a9ae0038d1ba7d4a124d4e063a9
XaiMendez <bmendez@bancoazul.com>
2018-06-06 15:17:02 -0600

    Adding CRUD functions

--- src/main/java/com/tminnova/api/controller/UserController.java
+++ src/main/java/com/tminnova/api/controller/UserController.java
@@ -5,12 +5,18 @@ package com.tminnova.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tminnova.api.model.User;
 import com.tminnova.api.repository.UserRepository;
+import com.tminnova.api.service.UserService;
 
 @RestController
 @RequestMapping("/user")
@@ -19,12 +25,32 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping("/")
-	@ResponseBody
-	public List<User> getAllUsers() {
-		//User User = new User(1L, "test", "test");
-		System.out.println(userRepository.findAll());
-		return userRepository.findAll();
+	 @Autowired
+	 private UserService userService;
+	
+	@GetMapping("/")
+	public List getAllUsers() {
+		return userService.findAll();
+	}
+	
+	@PostMapping("/")
+	public User createUser(@RequestBody User user) {	
+		return userService.create(user);
+	}
+	
+	@PutMapping("/")
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+	
+	@GetMapping(path = {"/{id}"})
+    public User findOne(@PathVariable("id") int id){
+        return userService.findById(id);
+    }
+	
+	@DeleteMapping(path ={"/{id}"})
+	public User deleteUser(@PathVariable("id") int id) {	
+		return userService.delete(id);
 	}
 
 }
