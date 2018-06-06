bda1a5ec17574dba1173c5298ffcdb0f2712c554
XaiMendez <bmendez@bancoazul.com>
2018-06-06 15:17:35 -0600

    Fixing user model

--- src/main/java/com/tminnova/api/model/User.java
+++ src/main/java/com/tminnova/api/model/User.java
@@ -1,15 +1,13 @@
 package com.tminnova.api.model;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,18 +17,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
-public class User implements Serializable{
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+public class User implements Serializable {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private int id;
-	
+
 	@Column(name = "username", nullable = false)
 	private String username;
-	
+
 	@Column(name = "password", nullable = false)
 	private String password;
+
+	public User() {
+		super();
+	}
 
 	public int getId() {
 		return id;
@@ -60,19 +63,14 @@ public class User implements Serializable{
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
 	}
-		
-	
-	/*
-	@Column(name = "enabled", nullable = false)
-	private boolean enabled;
-	*/
 
 	/*
-	 * @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-	private Set<Authorities> authorities = new HashSet<>();
-	*/
-	
+	 * @Column(name = "enabled", nullable = false) private boolean enabled;
+	 */
 
+	/*
+	 * @OneToMany(cascade = CascadeType.ALL, mappedBy = "user") private
+	 * Set<Authorities> authorities = new HashSet<>();
+	 */
 
-	
 }
